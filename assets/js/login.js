5ef993f60c2a761ba4d824b202d455adac11ef8c
itheima <itheima@itcast.cn>
2020-12-06 14:40:08 +0800

    添加了登录表单

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,4 +10,62 @@ $(function() {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+
+
+    // 从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+        // 通过 form.verify()函数自定义校验规则
+    form.verify({
+        // 自定义一个叫做pwd校验规则(写在了密码框中的lay-verify中)
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格'],
+        //校验两次密码是否一致
+        repwd: function(value) { // 通过形参拿到的是确认密码框中的内容
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+
+    //监听注册表单的提交事件
+    $('#form-reg').on('submit', function() {
+        e.preventDefault()
+        var data = {
+            username: $('#form_reg [name=username].val()'),
+            password: $('#form_reg [name=password].val()')
+        }
+
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功,请登录!')
+            $('#link_login').click()
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form-login').submit(function(e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                    // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                    // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
+
 })

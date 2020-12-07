04fdb6f636c1443186c979206b9d35b8ab2666b6
itheima <itheima@itcast.cn>
2020-12-07 18:09:34 +0800

    添加了JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,20 @@
 $(function() {
     getUserInfo()
+
+    var layer = layui.layer
+    $('#btnLogout').on('click', function() {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+                // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
 })
 
 function getUserInfo() {
@@ -14,7 +29,18 @@ function getUserInfo() {
                 return layui.layer.msg('获取用户信息失败')
             }
             renderAvatar(res.data)
-        }
+        },
+        // complete: function(res) {
+        //     // console.log('执行了 complete 回调：')
+        //     // console.log(res)
+        //     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 1. 强制清空 token
+        //         localStorage.removeItem('token')
+        //             // 2. 强制跳转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
     })
 }
 
